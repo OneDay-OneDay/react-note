db412e34541ab6311ab179bea39b4037719390d8
MrChangyaohua <471574253@qq.com>
2017-11-08 17:53:33 -0500

    container修改成功

--- public/javascripts/reducer/reducer.jsx
+++ public/javascripts/reducer/reducer.jsx
@@ -1,17 +1,20 @@
 "use strict";
 import { combineReducers } from "redux";
-import { INIT_NOTES, ADD_NOTE, DELETE_NOTE } from "../action/action.jsx";
+import { INIT_NOTES, ADD_NOTE, DELETE_NOTE, DISPLAYED_NOTE } from "../action/action.jsx";
 
 //处理笔记初始化、添加及删除请求
-function notes(state = [ ], action){
+function notes(state = [], action) {
 	//每一次的操作无论是添加、删除还是初始化，全部的笔记内容会被重新更新一次
-	switch(action.type){
+	switch (action.type) {
 		case INIT_NOTES:
-			return [ ...action.notes ];
+			return [...action.notes];
 		case ADD_NOTE:
-			return [ ...action.notes ];
+			return [...action.notes];
 		case DELETE_NOTE:
-			return [ ...action.notes ];
+			return [...action.notes];
+		case DISPLAYED_NOTE:
+			const formDisplayed = state.formDisplayed;
+			return { formDisplayed: !formDisplayed };
 		default:
 			return state;
 	}
